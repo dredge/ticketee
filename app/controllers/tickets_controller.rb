90b4e21a3a84e626f6e91be868fcc863f75f7ab6
Chris Eldredge <cjeasap@gmail.com>
2012-02-11 22:31:16 -0500

    Implemented deleting tickets feature

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,7 +1,7 @@
 class TicketsController < ApplicationController
 
   before_filter :find_project
-  before_filter :find_ticket, :only => [:show, :edit, :update, :deestroy]
+  before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
   
   def new
     @ticket = @project.tickets.build
@@ -32,6 +32,12 @@ class TicketsController < ApplicationController
       flash[:alert] = "Ticket has not been updated."
       render :action => "edit"
     end
+  end
+
+  def destroy
+    @ticket.destroy
+    flash[:notice] = "Ticket has been deleted."
+    redirect_to @project
   end
 
   private
